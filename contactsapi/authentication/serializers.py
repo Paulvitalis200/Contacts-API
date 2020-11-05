497d6cfedf15bd2613d3f7e9d1f74da3445425a5
Paulstar200 <vitalispaul48@live.com>
2020-11-05 13:39:45 +0300

    Update urls.py with routes

--- contactsapi/authentication/serializers.py
+++ contactsapi/authentication/serializers.py
@@ -1,22 +1,23 @@
-from rest_framework import seriailizers
+from rest_framework import serializers
 from django.contrib.auth.models import User
 
-class UserSerializer(seriailizers.ModelSerializer):
-    password = seriailizers.CharField(
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
         max_length=65,min_length=6,write_only=True)
-    email = seriailizers.EmailField(max_length=255,min_length=4)
-    first_name = seriailizers.CharField(max_length=255,min_length=2)
-    last_name = seriailizers.CharField(max_length=255,min_length=2)
+    email = serializers.EmailField(max_length=255,min_length=4)
+    first_name = serializers.CharField(max_length=255,min_length=2)
+    last_name = serializers.CharField(max_length=255,min_length=2)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
     def validate(self, attrs):
-        if User.objects.filter(email=attrs['email']).exists():
-            raise seriailizers.ValidationError(
-            {'email', ('Email is already in use')})
+        email = attrs.get('email', '')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError(
+            {'email': ('Email is already in use')})
         return super().validate(attrs)
 
     def create(self, validated_data):
-        return User.objects.create_user(validated_data)
+        return User.objects.create_user(**validated_data)
