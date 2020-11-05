aece2cea7668b04791a58a74028859ffbdd4a612
Paulstar200 <vitalispaul48@live.com>
2020-11-05 13:30:13 +0300

    Add our serializer to RegisterView

--- contactsapi/authentication/views.py
+++ contactsapi/authentication/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class RegisterView(GenericAPIView):
-     def post(self, request):
-         pass
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
